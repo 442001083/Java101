32fb41c7a4675cbd2f1189dfa08f11513ef4cd93
442001083 <91378343+442001083@users.noreply.github.com>
2021-09-30 20:30:36 +0300

    Create Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,27 @@
+ class Main
+{
+	public static void main(String[] args) {
+
+
+
+Scanner = new Scanner(System.in);
+float var1 =myScaner .nextint();
+float var2 = myScaner.nextint();
+System.out.println(var1+var2);
+System.out.println(var1-var2);
+System.out.println (var1*var2);
+System.out.println(var1/var2);
+System.out.println(var1%var2);
+System.out.println(var1>var2);
+System.out.println(var1>=var2);
+System.out.println(var1<var2);
+System.out.println(var1<=var2);
+System.out.println(var1!=var2);
+System.out.println(var1==var2);
+
+
+    }
+ { 
+
+Abdulrahman
+442001083
