cc93e8168d51ebdb23c5f743f5e6d462c73ecd91
442001083 <91378343+442001083@users.noreply.github.com>
2021-12-14 01:10:27 +0300

    Create weekend And week Days Switch.java

---
+++ weekend And week Days Switch.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+Scanner xyz = new Scanner(System.in);
+    System.out.println("the days");
+	int day = xyz.nextInt();
+	switch (day) {
+	    case 1:
+	   
+	   case 2:
+	   
+	   case 3:
+	       
+	   
+	   case 4:
+	   
+	  case 5: 
+	   
+	   System.out.println("weekdays");
+	   
+	   break;
+	   case 6:
+	   case 7:
+	   System.out.println("weekend");
+	   break;
+	   default:
+	   System.out.println("error");
+	   
+	}
+	
+	
+	    
+	    
+	    
+	}
+	
+}
