1c274faca7b79edc8745f9ce5331be700ea6f34e
442001083 <91378343+442001083@users.noreply.github.com>
2021-12-14 00:54:47 +0300

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner xyz = new Scanner(System.in);
+		System.out.println("enter a number ");
+		int num = xyz.nextInt();
+		if(num % 2==0)
+	System.out.println("Even");
+	
+	else
+	System.out.println("Add");
+	}
+}
