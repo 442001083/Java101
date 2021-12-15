fdbcf67e4aa93187d325af572c74fe88c3d7c691
442001083 <91378343+442001083@users.noreply.github.com>
2021-12-16 01:45:16 +0300

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,38 +1,31 @@
 import java.util.Random;
 import java.util.Scanner;
+public class  RockPaperSe {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String computerPlays = "rps";
+        Random rnd = new Random();
+        do{
+            System.out.println("If you want to play enter:\n(r or R) for rock,\n(p or P) for paper,\n(s or S) for scissors\nor Enter any key to exit");
+            
+            String user=scan.nextLine();
+            
+          user = (user.toLowerCase());
+           char user1=user.charAt(0);
+           if(user1!='r'&&user1!='s'&&user1!='p')
+           break;
+        
+          char computerChoice = computerPlays.charAt(rnd.nextInt(computerPlays.length()));
+           if((user1=='s'&&computerChoice=='r')||(user1=='p'&&computerChoice=='s')||(user1=='r'&&computerChoice=='p'))
+               System.out.println("computerPlays win");
+           
+           else if((user1=='r'&&computerChoice=='s')||(user1=='s'&&computerChoice=='p')||(user1=='p'&&computerChoice=='r'))
+               System.out.println("user win"); 
+            else 
+    System.out.println("Draw");
 
-public class RockPaperSe {
-
-public static void main(String[] args) {
-Scanner scan = new Scanner(System.in);
-String computerPlays = "rps";
-Random rnd = new Random();
-do{
-System.out.println("If you want to play enter:\n(r or R) for rock,\n(p or P) for paper,\n(s or S) for scissors\nor Enter any key to exit");
-String s="";
-if(scan.hasNextLine()){
-  s = scan.nextLine();
-}
-
-s=s.toLowerCase();
-char c=s.charAt(0);
-
-if(c!='r'&&c!='p'&&c!='s')
-break;
-else
-{
-    
-char computerChoice = computerPlays.charAt(rnd.nextInt(computerPlays.length()));
-if(c==computerChoice)
-{   
-    System.out.println(c+" won the match");
-}
-else
-continue;
-}
-
-
-System.out.println("\n\n");
-}while(true);
-}
+             System.out.println("\n\n");
+        }while(true);
+        
+    }
 }
