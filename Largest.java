5a81345d2268cdf990f376231af4305cbeafb55f
442001083 <91378343+442001083@users.noreply.github.com>
2021-12-16 02:54:51 +0300

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+int[] nums = {1,5,2,7,4,0,3};
+int largest =0;
+for (int index = 0; index < nums.length; index++){
+    if (nums[index] > largest){
+        largest = nums[index];
+    }
+}
+System.out.println(largest);
+	}
+}
