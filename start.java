55e7bf614a046979664fd0efbc9943165f634b71
442001083 <91378343+442001083@users.noreply.github.com>
2021-12-16 05:20:59 +0300

    Create start.java

---
+++ start.java
@@ -0,0 +1,25 @@
+public class Main
+
+{
+
+public static void main(String[] args) {
+
+for (int i = 0 ;i <= 6 ; i++)
+
+{
+
+for (int j = 0 ;j <= i ; j++)
+
+{
+
+System.out.print("*");
+
+}
+
+System.out.println();
+
+}
+
+}
+
+}
